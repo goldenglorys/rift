7abbe5010fffda0cb482ce5296e3661a245e413f
goldenglorys <olusolagloryolamide@gmail.com>
2024-08-20 03:24:02 +0300

    start of string anchor

--- app/main.py
+++ app/main.py
@@ -1,66 +1,3 @@
-# import sys
-
-# DIGIT = "\\d"
-# ALNUM = "\\w"
-# ANCHOR = "^"
-
-
-# def match_pattern(input_line, pattern):
-#     if pattern.startswith(ANCHOR):
-#         if not input_line or not match_pattern(input_line, pattern[1:]):
-#             return False
-#         return match_pattern(input_line[len(pattern) - 1 :], pattern[len(pattern) :])
-#     if len(input_line) == 0 and len(pattern) == 0:
-#         return True
-#     if not pattern:
-#         return True
-#     if not input_line:
-#         return False
-#     if pattern[0] == input_line[0]:
-#         return match_pattern(input_line[1:], pattern[1:])
-#     elif pattern[:2] == DIGIT:
-#         for i in range(len(input_line)):
-#             if input_line[i].isdigit():
-#                 return match_pattern(input_line[i:], pattern[2:])
-#         else:
-#             return False
-#     elif pattern[:2] == ALNUM:
-#         if input_line[0].isalnum():
-#             return match_pattern(input_line[1:], pattern[2:])
-#         else:
-#             return False
-#     elif pattern[0] == "[" and pattern[-1] == "]":
-#         if pattern[1] == ANCHOR:
-#             chrs = list(pattern[2:-1])
-#             for c in chrs:
-#                 if c in input_line:
-#                     return False
-#             return True
-#         chrs = list(pattern[1:-1])
-#         for c in chrs:
-#             if c in input_line:
-#                 return True
-#         return False
-#     else:
-#         return match_pattern(input_line[1:], pattern)
-
-
-# def main():
-#     pattern = sys.argv[2]
-#     input_line = sys.stdin.read()
-
-#     if sys.argv[1] != "-E":
-#         print("Expected first argument to be '-E'")
-#         exit(1)
-
-#     if match_pattern(input_line, pattern):
-#         exit(0)
-#     else:
-#         exit(1)
-
-
-# if __name__ == "__main__":
-#     main()
 import sys
 
 DIGIT = "\\d"
@@ -69,21 +6,21 @@ ANCHOR = "^"
 
 
 def match_pattern(input_line, pattern):
-    if pattern and pattern[0] == ANCHOR:
-        # If the pattern starts with ^, we can only match at the start of the line
-        if len(input_line) == 0 and len(pattern) == 1:
-            return True
-        if len(input_line) > 0 and pattern[1] == input_line[0]:
-            return match_pattern(input_line[1:], pattern[1:])
-        else:
-            return False
-    elif len(input_line) == 0 and len(pattern) == 0:
+    if len(input_line) == 0 and len(pattern) == 0:
         return True
-    elif not pattern:
+    if not pattern:
         return True
-    elif not input_line:
+    if not input_line:
         return False
-    elif pattern[0] == input_line[0]:
+    # Check for the start of string anchor '^'
+    if pattern[0] == ANCHOR:
+        if len(pattern) > 1 and pattern[1] == ANCHOR:
+            # Handle the case where '^' is followed by another '^'
+            return match_pattern(input_line, pattern[1:])
+        else:
+            # Ensure the input line starts with the rest of the pattern
+            return input_line.startswith(pattern[1:])
+    if pattern[0] == input_line[0]:
         return match_pattern(input_line[1:], pattern[1:])
     elif pattern[:2] == DIGIT:
         for i in range(len(input_line)):
@@ -97,7 +34,7 @@ def match_pattern(input_line, pattern):
         else:
             return False
     elif pattern[0] == "[" and pattern[-1] == "]":
-        if pattern[1] == "^":
+        if pattern[1] == ANCHOR:
             chrs = list(pattern[2:-1])
             for c in chrs:
                 if c in input_line:
